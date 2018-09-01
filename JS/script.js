0daea38420324bed44d3c594fb3436c6851852c1
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-08-31 19:29:26 -0700

    no message

--- JS/script.js
+++ JS/script.js
@@ -100,3 +100,27 @@ $(document).ready(function () {
 
     });
 });
+
+$(function () {
+    $('.nav__icon').on('click', function () {
+        $(this).toggleClass('active');
+        $('nav ul').slideToggle();
+    });
+
+    $(window).on('load resize', function () {
+        var w = $(window).width();
+        var h = $(window).height();
+        var x = 767;
+        if (w >= x) {
+            $('nav ul').css({
+                display: 'flex',
+                height: 'auto'
+            });
+        } else {
+            $('nav ul').css({
+                display: 'none',
+                height: h + 'px'
+            });
+        }
+    });
+});
