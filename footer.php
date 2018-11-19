430326c1f58d0289b22459ae682488035a73de5c
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-11-19 10:09:20 -0800

    small modify

--- footer.php
+++ footer.php
@@ -2,8 +2,5 @@
 <script>
 </script>
 <?php wp_footer(); ?>
-<!-- <script type="text/javascript" src="http://localhost/wp-content/themes/portfolio/js/fullpage.js?ver=4.9.8"></script>
-<script type="text/javascript" src="http://localhost/wp-content/themes/portfolio/js/script.js?ver=4.9.8"></script>
-<script type="text/javascript" src="js/fullpage.js"></script> -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,23 +5,23 @@
 
 <head>
 <!-- General -->
-<meta property="og:title" content="Nagisa Kojima" />
+<!-- <meta property="og:title" content="Nagisa Kojima" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://nagisa-kojima.com/" />
 <meta property="og:image" content="サムネイル画像のURL" />
 <meta property="og:site_name"  content="Nagisa Kojima" />
 <meta property="og:description" content="Nagisa Kojima Personal Portfolio Website" />
 
-<!-- Facebook -->
-<meta property="fb:app_id" content="1008599752676821" />
+ Facebook -->
+<!-- <meta property="fb:app_id" content="1008599752676821" />
 
-<!-- Twitter用 -->
-<meta name="twitter:card" content="summary_large_image" />
+Twitter用 -->
+<!-- <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@Twitter" />
 <meta name="twitter:title" content="Nagisa Kojima" />
 <meta name="twitter:url" content="https://nagisa-kojima.com/" />
 <meta name="twitter:description" content="Nagisa Kojima Personal Portfolio Website" />
-<meta name="twitter:image" content="サムネイル画像のURL" />
+<meta name="twitter:image" content="サムネイル画像のURL" /> --> 
   <link rel="icon" href="img/favicon.ico" />
   <title>Nagisa Kojima</title>
   <?php wp_head(); ?>
