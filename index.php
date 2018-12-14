eecf46f501100e3e56aeb2b722c90517403a39da
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-12-14 00:02:09 -0800

    svg logo modified

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <nav class="nav-bar first-page-nav">
      <ul class="menu">
-         <li id="mymenu" class="menu-list"><a href="<?php echo esc_url(home_url()); ?>/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg"></li>
+         <li id="mymenu" class="menu-list"><a href="<?php echo esc_url(home_url()); ?>/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg"></a></li>
          <li class="menu-list"><a href="<?php echo esc_url(home_url()); ?>/about">About</a></li>
          <li class="menu-list"><a href="<?php echo esc_url(home_url()); ?>/works">Works</a></li>
         </ul>
@@ -10,7 +10,7 @@
   <!-- =========== navigation bar=================-->
   <div class="top-border-rect">
     <p class="btn__box">
-      <a href="#"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg"></a>
+      <a href="#"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg"></a></a>
     </p>
   </div>
 
