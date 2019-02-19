4af4e4534554eee0a23d4b65b67c693d8f064b50
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-02-18 22:17:01 -0800

    sass comment removed

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-// SassとCssの保存先を指定
 gulp.task("sass", function() {
   gulp
     .src("./sass/**/*.scss")
@@ -13,11 +12,10 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("./css/"));
 });
 
-//自動監視のタスクを作成(sass-watchと名付ける)
+
 gulp.task("sass-watch", ["sass"], function() {
   var watcher = gulp.watch("./sass/**/*.scss", ["sass"]);
   watcher.on("change", function(event) {});
 });
 
-// タスク"task-watch"がgulpと入力しただけでdefaultで実行されるようになる
 gulp.task("default", ["sass-watch"]);
